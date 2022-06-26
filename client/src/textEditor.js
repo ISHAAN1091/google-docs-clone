68c37977cef986135577f479fdc3e9d3c80cc9c6
Ishaan <ishaan1091@gmail.com>
2022-06-26 14:02:13 +0530

    Added Multiple Documents Functionality

--- client/src/textEditor.js
+++ client/src/textEditor.js
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,7 @@ const TOOLBAR_OPTIONS = [
 ];
 
 export default function TextEditor() {
+    const { id: documentId } = useParams();
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
 
@@ -26,6 +28,17 @@ export default function TextEditor() {
             s.disconnect();
         };
     }, []);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        socket.once("load-document", (document) => {
+            quill.setContents(document);
+            quill.enable();
+        });
+
+        socket.emit("get-document", documentId);
+    }, [socket, quill, documentId]);
 
     useEffect(() => {
         if (socket == null || quill == null) return;
@@ -56,12 +69,13 @@ export default function TextEditor() {
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        setQuill(
-            new Quill(editor, {
-                theme: "snow",
-                modules: { toolbar: TOOLBAR_OPTIONS },
-            })
-        );
+        const q = new Quill(editor, {
+            theme: "snow",
+            modules: { toolbar: TOOLBAR_OPTIONS },
+        });
+        setQuill(q);
+        q.disable();
+        q.setText("Loading...");
     }, []);
 
     return <div className="container" ref={wrapperRef}></div>;
